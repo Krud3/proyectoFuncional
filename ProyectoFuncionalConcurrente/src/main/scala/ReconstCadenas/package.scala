1d8ab52da285232f61151b6b230541214528d0c8
JuanC513 <juan.esteban.clavijo@correounivalle.edu.co>
2023-11-25 13:57:51 -0500

    Actualización con las funciones ingenua y mejorada

--- ProyectoFuncionalConcurrente/src/main/scala/ReconstCadenas/package.scala
+++ ProyectoFuncionalConcurrente/src/main/scala/ReconstCadenas/package.scala
@@ -1,3 +1,80 @@
-package object ReconstCadenas {
+import ArbolSufijos._
+import Oraculo._
 
+package object ReconstCadenas {
+  def reconstruirCadenaIngenuo(n: Int, o: Oraculo): Seq[Char] = {
+    // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    if (n < 1) {
+      Seq.empty[Char]
+    }
+    else {
+      def subcadenas(alfabeto: Seq[Char], longitud: Int): List[Seq[Char]] = {
+        if (longitud == 0)
+          Seq.empty[Char] :: Nil
+        else {
+          val subcadenas_anteriores = subcadenas(alfabeto, longitud - 1)
+          val lista_subcadenas = for (un_caracter <- alfabeto; una_subcadena <- subcadenas_anteriores) yield un_caracter +: una_subcadena
+          lista_subcadenas.toList
+        }
+      }
+
+      val cadenas = subcadenas(alfabeto, n)
+      val primera_cadena = cadenas.find(o(_)).get
+
+      primera_cadena
+    }
+  }
+
+  def reconstruirCadenaMejorado(n: Int, o: Oraculo): Seq[Char] = {
+    // recibe la longitud de la secuencia que hay que reconstruir (n), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa la propiedad de que si s = s1 ++ s2 entonces s1 y s2 tambien son subsecuencias de s
+
+    if (n < 1) {
+      Seq.empty[Char]
+    }
+    else {
+      def subcadenas(alfabeto: Seq[Char], longitud: Int): List[Seq[Char]] = {
+        if (longitud == 0)
+          Seq.empty[Char] :: Nil
+        else {
+          val subcadenas_validas_anteriores = subcadenas(alfabeto, longitud - 1)
+          val lista_combinaciones = for (un_caracter <- alfabeto; una_subcadena <- subcadenas_validas_anteriores if o(un_caracter +: una_subcadena)) yield un_caracter +: una_subcadena
+          lista_combinaciones.toList
+        }
+      }
+
+      val cadenas = subcadenas(alfabeto, n)
+      val primera_cadena = cadenas.find(o(_)).get
+
+      primera_cadena
+    }
+  }
+
+  def reconstruirCadenaTurbo(n: Int, o: Oraculo): Seq[Char] = {
+    // recibe la longitud de la secuencia que hay que reconstruir (n, potencia de 2), y un oraculo para esa secuencia
+    // y devuelve la secuencia reconstruida
+    // Usa la propiedad de que si s = s1 ++ s2 entonces s1 y s2 tambien son subsecuencias de s
+
+    if (n < 1) {
+      Seq.empty[Char]
+    }
+    else {
+      def subcadenasTurbo(alfabeto: Seq[Char], longitud: Int): List[Seq[Char]] = {
+        if (longitud == 1)
+          (for (un_caracter <- alfabeto) yield un_caracter +: Nil).toList
+        else {
+          val subcadenas_validas_anteriores = subcadenasTurbo(alfabeto, longitud/2)
+          val lista_combinaciones = for (sub_cadena_1 <- subcadenas_validas_anteriores; sub_cadena_2 <- subcadenas_validas_anteriores if o(sub_cadena_1 ++ sub_cadena_2)) yield sub_cadena_1 ++ sub_cadena_2
+          lista_combinaciones
+        }
+      }
+
+      val cadenas = subcadenasTurbo(alfabeto, n)
+      val primera_cadena = cadenas.find(o(_)).get
+
+      primera_cadena
+    }
+  }
 }
--- ProyectoFuncionalConcurrente/src/test/scala/Pruebas.sc
+++ ProyectoFuncionalConcurrente/src/test/scala/Pruebas.sc
@@ -1,30 +1,38 @@
-//import Oraculo._//
-//import ReconstCadenas._
-//import scala.util.Random
+import Oraculo._
+import ReconstCadenas._
+import scala.util.Random
 
-//val random = new Random()
+val random = new Random()
 
-//def secAlAzar(long:Int, s:Seq[Char]): Seq[Char] = {//
-  //Crea una secuencia de long caracteres del alfabeto,
+def secAlAzar(long:Int, s:Seq[Char]): Seq[Char] = {
+  // Crea una secuencia de long caracteres del alfabeto,
   // escogidos de forma aleatoria, terminando en s
-//  if (s.length==long) s
-//  else {
-//    val indiceAzar=random.nextInt(4)
-//    secAlAzar(long,alfabeto(indiceAzar)+:s)
-//  }
-//}
-//val costoOraculo = 1
+  if (s.length==long) s
+  else {
+    val indiceAzar=random.nextInt(4)
+    secAlAzar(long,alfabeto(indiceAzar)+:s)
+  }
+}
 
-//val sec1=Seq('a', 'c', 'c', 'a')
-//val sec2 = Seq('a', 'c', 'g', 'c', 'a')
-//val sec3=secAlAzar(10,Seq())
+val costoOraculo = 1
 
-//val or_1=crearOraculo(costoOraculo)(sec1)
-//val or_2=crearOraculo(costoOraculo)(sec2)
-//val or_3=crearOraculo(costoOraculo)(sec3)
+val sec1=Seq('a', 'c', 'c', 'a')
+val sec2 = Seq('a', 'c', 'g', 'c', 'a')
+val sec3=secAlAzar(10,Seq())
 
-//reconstruirCadenaIngenuo(sec1.length, or_1)
-//reconstruirCadenaIngenuo(sec2.length, or_2)
+val or_1=crearOraculo(costoOraculo)(sec1)
+val or_2=crearOraculo(costoOraculo)(sec2)
+val or_3=crearOraculo(costoOraculo)(sec3)
+
+reconstruirCadenaIngenuo(sec1.length, or_1)
+reconstruirCadenaIngenuo(sec2.length, or_2)
+
+val sec4 = secAlAzar(8,Seq())
+val or_4 = crearOraculo(costoOraculo)(sec4)
+
+reconstruirCadenaMejorado(sec4.length, or_4)
+reconstruirCadenaTurbo(sec4.length, or_4)
+
 //reconstruirCadenaIngenuo(sec3.length, or_3)
 
 //def secsCortasParaPruebas(n:Int):Seq[Seq[Char]] = for {//
