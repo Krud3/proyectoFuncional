a15f41abcadca0da396138faccc0559c4c23aece
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2023-12-06 19:50:22 -0500

    Se actualiza el bench y la main para correr bien los datos

--- ProyectoFuncionalConcurrente/src/main/scala/Benchmark/package.scala
+++ ProyectoFuncionalConcurrente/src/main/scala/Benchmark/package.scala
@@ -20,8 +20,45 @@ package object Benchmark {
             KeyValue(Key.verbose -> false)
         ) withWarmer(new Warmer.Default) measure {a2(tam,o)}
 
+        
+
         val speedUp = timeA1.value / timeA2.value
         
         (timeA1.value,timeA2.value,speedUp)
+    }
+
+    def comparar2(a1:Algoritmo,a2:Algoritmo)(tam:Int,o:Oraculo):(Double,Double,Double) = {
+        
+        
+        val inicio1 = System.nanoTime()
+        a1(tam,o)
+        val fin1 = System.nanoTime()
+        val tiempo1 = (fin1 - inicio1)
+        val tiempoSeg1 = tiempo1/1e9d
+        
+        val inicio2 = System.nanoTime()
+        a2(tam,o)
+        val fin2 = System.nanoTime()
+        val tiempo2 = (fin2 - inicio2)
+        val tiempoSeg2 = tiempo2/1e9d
+
+        
+
+        val speedUp = tiempoSeg1 / tiempoSeg2
+        
+        (tiempoSeg1,tiempoSeg2,speedUp)
+    }
+
+    def compararSola(a1:Algoritmo)(tam:Int,o:Oraculo):(Double) = {
+        
+        
+        val inicio1 = System.nanoTime()
+        a1(tam,o)
+        val fin1 = System.nanoTime()
+        val tiempo1 = (fin1 - inicio1)
+        val tiempoSeg1 = tiempo1/1e9d
+
+        
+        (tiempoSeg1)
     }
 }
--- ProyectoFuncionalConcurrente/src/main/scala/Main.scala
+++ ProyectoFuncionalConcurrente/src/main/scala/Main.scala
@@ -8,22 +8,21 @@ import java.io.{BufferedWriter, FileWriter}
 object Main {
     def main(args: Array[String]): Unit = {       
 
-        val resultadosIngenuoMejorado = automaticBenchSmall(reconstruirCadenaIngenuo,reconstruirCadenaMejorado,List(),1,10)
-        guardarResultadosEnArchivo(resultadosIngenuoMejorado, "IvsfdsfadM1to32.txt")
-        //resultadosIngenuoMejorado.foreach(println)
-        println("termino ingenuo mejorado")
-        val resultadosMejoradoTurbo =automaticBenchLarge(reconstruirCadenaMejorado,reconstruirCadenaTurbo,List(),2,1024)
-        guardarResultadosEnArchivo(resultadosMejoradoTurbo, "MvsT2to1024.txt")
+        val resultadosIngenuoMejorado = automaticBenchSmallIngenua(reconstruirCadenaIngenuo,List(),1,16)
+        guardarResultadosEnArchivoIngenuo(resultadosIngenuoMejorado, "Ingenuo1to16.txt")
+        println("termino ingenuo ")
+        val resultadosMejoradoTurbo =automaticBenchLarge2(reconstruirCadenaMejorado,reconstruirCadenaTurbo,List(),0,1024)
+        guardarResultadosEnArchivo(resultadosMejoradoTurbo, "MejoradovsTurbo0to1024.txt")
         //resultadosMejoradoTurbo.foreach(println)
         println("termino mejorado turbo")
-        val resultadosTurboTurboMejorado =automaticBenchLarge(reconstruirCadenaTurbo,reconstruirCadenaTurboMejorado,List(),2,1024)
-        guardarResultadosEnArchivo(resultadosTurboTurboMejorado, "TvsTM2to1024.txt")
+        /*val resultadosTurboTurboMejorado =automaticBenchLarge(reconstruirCadenaTurbo,reconstruirCadenaTurboMejorado,List(),2,512)
+        guardarResultadosEnArchivo(resultadosTurboTurboMejorado, "TvsTM2to512.txt")
         //resultadosTurboTurboMejorado.foreach(println)
         println("termino turbo mejorado")
-        val resultadosTurboMejoradoTurboAcelerada =automaticBenchLarge(reconstruirCadenaTurboMejorado,reconstruirCadenaTurboAcelerada,List(),2,4096)
-        guardarResultadosEnArchivo(resultadosTurboMejoradoTurboAcelerada, "TMvsTA2to4096.txt")
+        val resultadosTurboMejoradoTurboAcelerada =automaticBenchLarge(reconstruirCadenaTurboMejorado,reconstruirCadenaTurboAcelerada,List(),2,2048)
+        guardarResultadosEnArchivo(resultadosTurboMejoradoTurboAcelerada, "TMvsTA2to2048.txt")
         //resultadosTurboMejoradoTurboAcelerada.foreach(println)
-        println("termino turbo mejorado turbo acelerada")
+        println("termino turbo mejorado turbo acelerada")*/
     }
 
     val costoOraculo = 1
@@ -51,6 +50,31 @@ object Main {
         }        
     }
     
+    def automaticBenchSmall2(a1:Algoritmo,a2:Algoritmo,resultados:List[(Double,Double,Double)],aumentador:Int, max:Int): List[(Double,Double,Double)] = {
+        if(aumentador > max){
+            resultados
+        } else {
+            val s = secAlAzar(aumentador,Seq())
+            val tam = s.length
+            val o = crearOraculo(costoOraculo)(s)
+            val resultado = comparar2(a1,a2)(tam, o)
+            println(resultado)
+            automaticBenchSmall2(a1,a2,resultados++List(resultado),aumentador+1,max)
+        }        
+    }
+
+    def automaticBenchSmallIngenua(a1:Algoritmo,resultados:List[(Double)],aumentador:Int, max:Int): List[(Double)] = {
+        if(aumentador > max){
+            resultados
+        } else {
+            val s = secAlAzar(aumentador,Seq())
+            val tam = s.length
+            val o = crearOraculo(costoOraculo)(s)
+            val resultado = compararSola(a1)(tam, o)
+            println(resultado)
+            automaticBenchSmallIngenua(a1,resultados++List(resultado),aumentador+1,max)
+        }        
+    }
     def automaticBenchLarge(a1:Algoritmo,a2:Algoritmo,resultados:List[(Double,Double,Double)],aumentador:Int, max:Int): List[(Double,Double,Double)] = {
         println(aumentador)
         println(max)
@@ -66,7 +90,35 @@ object Main {
         }        
     }
 
+    def automaticBenchLarge2(a1:Algoritmo,a2:Algoritmo,resultados:List[(Double,Double,Double)],aumentador:Int, max:Int): List[(Double,Double,Double)] = {
+        println(aumentador)
+        println(max)
+        val potencia = math.pow(2,aumentador).toInt
+        if(potencia > max){
+            resultados
+        } else {
+            val s = secAlAzar(potencia,Seq())
+            val tam = s.length
+            val o = crearOraculo(costoOraculo)(s)
+            val resultado = comparar2(a1,a2)(tam, o)
+            println(resultado)
+            automaticBenchLarge2(a1,a2,resultados++List(resultado),aumentador + 1,max)
+        }        
+    }
+
     def guardarResultadosEnArchivo(resultados: List[(Double, Double, Double)], nombreArchivo: String): Unit = {
+        val archivo = new BufferedWriter(new FileWriter(nombreArchivo))
+        try {
+            resultados.foreach { resultado =>
+                archivo.write(resultado.toString())
+                archivo.newLine()
+                }
+        } finally {
+            archivo.close()
+        }
+    }
+
+    def guardarResultadosEnArchivoIngenuo(resultados: List[(Double)], nombreArchivo: String): Unit = {
         val archivo = new BufferedWriter(new FileWriter(nombreArchivo))
         try {
             resultados.foreach { resultado =>
